0fe5e74d088885629f703d76326f93216055d728
federicopigozzi <pigozzife@gmail.com>
2019-11-27 10:27:56 +0100

    from HashMap to LinkedHashMap

--- production/source/dssc/fizzbuzz/FizzBuzz.java
+++ production/source/dssc/fizzbuzz/FizzBuzz.java
@@ -1,16 +1,16 @@
 package dssc.fizzbuzz;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
 
-    public void printNumbers(HashMap<Integer, String> codes) {
+    public void printNumbers(LinkedHashMap<Integer, String> codes) {
         IntStream seq = IntStream.rangeClosed(1, 100);
         seq.mapToObj(x -> numToString(x, codes)).forEach(System.out::println);
     }
 
-    public static String numToString(int num, HashMap<Integer, String> codes) {
+    public static String numToString(int num, LinkedHashMap<Integer, String> codes) {
         String ans = "";
         for (Integer divisor : codes.keySet()) {
             if (num % divisor == 0) {
