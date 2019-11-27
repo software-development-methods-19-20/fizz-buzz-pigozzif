a9880551f1d13f0bdd00e3f27eec3530e7379d43
federicopigozzi <pigozzife@gmail.com>
2019-11-27 10:45:16 +0100

    from LinkedHashMap to TreeMap

--- production/source/dssc/fizzbuzz/FizzBuzz.java
+++ production/source/dssc/fizzbuzz/FizzBuzz.java
@@ -1,16 +1,16 @@
 package dssc.fizzbuzz;
 
-import java.util.LinkedHashMap;
+import java.util.TreeMap;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
 
-    public void printNumbers(LinkedHashMap<Integer, String> codes) {
+    public void printNumbers(TreeMap<Integer, String> codes) {
         IntStream seq = IntStream.rangeClosed(1, 100);
         seq.mapToObj(x -> numToString(x, codes)).forEach(System.out::println);
     }
 
-    public static String numToString(int num, LinkedHashMap<Integer, String> codes) {
+    public static String numToString(int num, TreeMap<Integer, String> codes) {
         String ans = "";
         for (Integer divisor : codes.keySet()) {
             if (num % divisor == 0) {
